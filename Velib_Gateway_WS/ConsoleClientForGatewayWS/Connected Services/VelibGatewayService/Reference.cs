496068a2d3708618efa26f43ae9413b7b2486c90
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2018-03-15 11:55:42 +0100

    Added console client

---
+++ Velib_Gateway_WS/ConsoleClientForGatewayWS/Connected Services/VelibGatewayService/Reference.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//     //
+//     Les changements apportés à ce fichier peuvent provoquer un comportement incorrect et seront perdus si
+//     le code est regénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace VelibGatewayService
+{
+    
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName="VelibGatewayService.IVelibService")]
+    internal interface IVelibService
+    {
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IVelibService/GetStations", ReplyAction="http://tempuri.org/IVelibService/GetStationsResponse")]
+        System.Threading.Tasks.Task<string[]> GetStationsAsync(string city);
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IVelibService/GetCities", ReplyAction="http://tempuri.org/IVelibService/GetCitiesResponse")]
+        System.Threading.Tasks.Task<string[]> GetCitiesAsync();
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IVelibService/GetAvailableVelibs", ReplyAction="http://tempuri.org/IVelibService/GetAvailableVelibsResponse")]
+        System.Threading.Tasks.Task<int> GetAvailableVelibsAsync(string stationName);
+    }
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    internal interface IVelibServiceChannel : VelibGatewayService.IVelibService, System.ServiceModel.IClientChannel
+    {
+    }
+    
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    internal partial class VelibServiceClient : System.ServiceModel.ClientBase<VelibGatewayService.IVelibService>, VelibGatewayService.IVelibService
+    {
+        
+    /// <summary>
+    /// Implémentez cette méthode partielle pour configurer le point de terminaison de service.
+    /// </summary>
+    /// <param name="serviceEndpoint">Point de terminaison à configurer</param>
+    /// <param name="clientCredentials">Informations d'identification du client</param>
+    static partial void ConfigureEndpoint(System.ServiceModel.Description.ServiceEndpoint serviceEndpoint, System.ServiceModel.Description.ClientCredentials clientCredentials);
+        
+        public VelibServiceClient() : 
+                base(VelibServiceClient.GetDefaultBinding(), VelibServiceClient.GetDefaultEndpointAddress())
+        {
+            this.Endpoint.Name = EndpointConfiguration.BasicHttpBinding_IVelibService.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public VelibServiceClient(EndpointConfiguration endpointConfiguration) : 
+                base(VelibServiceClient.GetBindingForEndpoint(endpointConfiguration), VelibServiceClient.GetEndpointAddress(endpointConfiguration))
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public VelibServiceClient(EndpointConfiguration endpointConfiguration, string remoteAddress) : 
+                base(VelibServiceClient.GetBindingForEndpoint(endpointConfiguration), new System.ServiceModel.EndpointAddress(remoteAddress))
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public VelibServiceClient(EndpointConfiguration endpointConfiguration, System.ServiceModel.EndpointAddress remoteAddress) : 
+                base(VelibServiceClient.GetBindingForEndpoint(endpointConfiguration), remoteAddress)
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public VelibServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
+                base(binding, remoteAddress)
+        {
+        }
+        
+        public System.Threading.Tasks.Task<string[]> GetStationsAsync(string city)
+        {
+            return base.Channel.GetStationsAsync(city);
+        }
+        
+        public System.Threading.Tasks.Task<string[]> GetCitiesAsync()
+        {
+            return base.Channel.GetCitiesAsync();
+        }
+        
+        public System.Threading.Tasks.Task<int> GetAvailableVelibsAsync(string stationName)
+        {
+            return base.Channel.GetAvailableVelibsAsync(stationName);
+        }
+        
+        public virtual System.Threading.Tasks.Task OpenAsync()
+        {
+            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
+        }
+        
+        public virtual System.Threading.Tasks.Task CloseAsync()
+        {
+            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
+        }
+        
+        private static System.ServiceModel.Channels.Binding GetBindingForEndpoint(EndpointConfiguration endpointConfiguration)
+        {
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_IVelibService))
+            {
+                System.ServiceModel.BasicHttpBinding result = new System.ServiceModel.BasicHttpBinding();
+                result.MaxBufferSize = int.MaxValue;
+                result.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+                result.MaxReceivedMessageSize = int.MaxValue;
+                result.AllowCookies = true;
+                return result;
+            }
+            throw new System.InvalidOperationException(string.Format("Le point de terminaison nommé \'{0}\' est introuvable.", endpointConfiguration));
+        }
+        
+        private static System.ServiceModel.EndpointAddress GetEndpointAddress(EndpointConfiguration endpointConfiguration)
+        {
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_IVelibService))
+            {
+                return new System.ServiceModel.EndpointAddress("http://localhost:21318/VelibGatewayService.svc");
+            }
+            throw new System.InvalidOperationException(string.Format("Le point de terminaison nommé \'{0}\' est introuvable.", endpointConfiguration));
+        }
+        
+        private static System.ServiceModel.Channels.Binding GetDefaultBinding()
+        {
+            return VelibServiceClient.GetBindingForEndpoint(EndpointConfiguration.BasicHttpBinding_IVelibService);
+        }
+        
+        private static System.ServiceModel.EndpointAddress GetDefaultEndpointAddress()
+        {
+            return VelibServiceClient.GetEndpointAddress(EndpointConfiguration.BasicHttpBinding_IVelibService);
+        }
+        
+        public enum EndpointConfiguration
+        {
+            
+            BasicHttpBinding_IVelibService,
+        }
+    }
+}
---
+++ Velib_Gateway_WS/ConsoleClientForGatewayWS/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using VelibGatewayService;
+namespace ConsoleClientForGatewayWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console client for VelibGatewayWS (async)");
+            Console.WriteLine("see help for commands");
+            Console.Write("#> ");
+            string userinput = Console.ReadLine();
+            while (true)
+            {
+                process(userinput);
+                Console.Write("#> ");
+                userinput = Console.ReadLine();
+            }
+        }
+
+        static async void process(string input)
+        {
+            if (input.Contains("getcities"))
+            {
+                String[] cities = await new VelibServiceClient().GetCitiesAsync();
+                foreach (string city in cities)
+                {
+                    Console.Write(city + " ");
+                }
+                Console.Write("\n#> ");
+            }
+            else if (input.Contains("getstations"))
+
+            {
+                string city = null;
+                try
+                {
+                    city = input.Split(" ")[1];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("bad syntax, see help for details");
+                }
+
+                String[] stations = null;
+                try
+                {
+                   stations = await new VelibServiceClient().GetStationsAsync(city);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("bad city provided");
+                    Console.Write("#> ");
+                    return;
+                }
+                foreach (string station in stations)
+                {
+                    Console.WriteLine(station + " ");
+                }
+                Console.Write("#> ");
+            }
+            else if (input.Contains("getbikes"))
+            {
+                int bikes = 0;
+                try
+                {
+                    bikes = await new VelibServiceClient().GetAvailableVelibsAsync(input.Split("getbikes")[0]);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("bad station provided");
+                    Console.Write("#> ");
+                    return;
+                }
+                Console.WriteLine("Available bikes for " + input.Split("getbikes")[0]+": "+bikes);
+                Console.Write("#> ");
+            }
+            else if (input.Contains("help"))
+            {
+                Console.WriteLine("Available commands:");
+                Console.WriteLine("getcities - Retrieves the available cities");
+                Console.WriteLine("getstations <city> - Lists the stations name for the city");
+                Console.WriteLine("getbikes <stationName> - Gets the number of the available bikes for the station provided");
+            }
+            else if (input.Contains("exit"))
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("bad command, see help for details");
+
+            }
+        }
+    }
+}
